d39ad4307885ee5ab838bce10d2f06003c5aa13f
hwigyeompark <phg5590@gmail.com>
2018-07-12 20:48:39 +0900

    origin
    Add recyclePattern

--- create-map/Assets/script/GameManager.cs
+++ create-map/Assets/script/GameManager.cs
@@ -1,21 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
 	public List<GameObject> prefabList = new List<GameObject>();
-	public List<GameObject> mapLogList;
+	public List<GameObject> mapLogList = new List<GameObject>();
 	public Vector3 nextPosition;
-	public string code;
+	public string[] codeArr = new string[100];
 	public int direction;
-	public char[] codeArray;
 	private int firstMapSize;
+	GameObject[] gameObjectArr = new GameObject[100];
 
 	void Start () {
 		direction = 0;
 		firstMapSize = 5;
-		codeArray = code.ToCharArray();
 		StartCoroutine ("initiateMap");
 	}
 		
@@ -23,25 +24,35 @@ public class GameManager : MonoBehaviour {
 
 	}
 
-	IEnumerator loadPrefab(){
-		GameObject curveLeft = Instantiate (Resources.Load ("/Prefabs/curveLeft")) as GameObject;
-		prefabList.Add(curveLeft);
-		GameObject curveRight = Instantiate (Resources.Load ("/Prefabs/curveRight")) as GameObject;
-		prefabList.Add(curveRight);
-		GameObject direct = Instantiate (Resources.Load ("/Prefabs/direct")) as GameObject;
-		prefabList.Add(direct);
-		GameObject downHill = Instantiate (Resources.Load ("/Prefabs/downHill")) as GameObject;
-		prefabList.Add(downHill);
-		GameObject downHillLow = Instantiate (Resources.Load ("/Prefabs/downHillLow")) as GameObject;
-		prefabList.Add(downHillLow);
-		GameObject turnLeft = Instantiate (Resources.Load ("/Prefabs/turnLeft")) as GameObject;
-		prefabList.Add(turnLeft);
-		GameObject turnRight = Instantiate (Resources.Load ("/Prefabs/turnRight")) as GameObject;
-		prefabList.Add(turnRight);
-		GameObject upHill = Instantiate (Resources.Load ("/Prefabs/upHill")) as GameObject;
-		prefabList.Add(upHill);
-		GameObject upHillLow = Instantiate (Resources.Load ("/Prefabs/upHillLow")) as GameObject;
-		prefabList.Add(upHillLow);
+//	IEnumerator loadPrefab(){
+//		GameObject curveLeft = Instantiate (Resources.Load ("/Prefabs/curveLeft")) as GameObject;
+//		prefabList.Add(curveLeft);
+//		GameObject curveRight = Instantiate (Resources.Load ("/Prefabs/curveRight")) as GameObject;
+//		prefabList.Add(curveRight);
+//		GameObject direct = Instantiate (Resources.Load ("/Prefabs/direct")) as GameObject;
+//		prefabList.Add(direct);
+//		GameObject downHill = Instantiate (Resources.Load ("/Prefabs/downHill")) as GameObject;
+//		prefabList.Add(downHill);
+//		GameObject downHillLow = Instantiate (Resources.Load ("/Prefabs/downHillLow")) as GameObject;
+//		prefabList.Add(downHillLow);
+//		GameObject turnLeft = Instantiate (Resources.Load ("/Prefabs/turnLeft")) as GameObject;
+//		prefabList.Add(turnLeft);
+//		GameObject turnRight = Instantiate (Resources.Load ("/Prefabs/turnRight")) as GameObject;
+//		prefabList.Add(turnRight);
+//		GameObject upHill = Instantiate (Resources.Load ("/Prefabs/upHill")) as GameObject;
+//		prefabList.Add(upHill);
+//		GameObject upHillLow = Instantiate (Resources.Load ("/Prefabs/upHillLow")) as GameObject;
+//		prefabList.Add(upHillLow);
+//	}
+//
+	
+	IEnumerator searchPatternCorrect(){
+		for (int i = 0; i < gameObjectArr.Length; i++) {
+			if (codeArr.GetValue()) {
+				GameObject recyclePattern = Instantiate (prefabList [int.Parse (code.Substring(i, 1))], new Vector3 (i * 6.0F, 0, 0), Quaternion.identity);
+				mapLogList.Add (recyclePattern);
+			}
+		}
 	}
 
 	IEnumerator initiateMap()
@@ -66,12 +77,13 @@ public class GameManager : MonoBehaviour {
 	IEnumerator createMap()
 	{
 		/*할거*/
-		for(int i=0;i<prefabList.Count;i++){
-
+		for(int i=firstMapSize;i<code.Length;i++){
 			yield return new WaitForSeconds(0.5f); //딜레이
-			GameObject pattern = Instantiate(prefabList[i], new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
-			mapLogList.Add (pattern);
-			StartCoroutine("aaa");
+			if (prefabList.Count > int.Parse (code.Substring(i, 1)))
+			{
+				GameObject pattern = Instantiate(prefabList [int.Parse (code.Substring(i, 1))], new Vector3(i * 6.0F, 0, 0), Quaternion.identity);
+				mapLogList.Add (pattern);
+			}
 		}
 	}
 }
